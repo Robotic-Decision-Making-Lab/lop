284a26b7b7281b7b32cdff6362e8b7e89a447238
ianran <rankini@oregonstate.edu>
2023-12-06 16:47:47 -0800

    Removed testing of probits not currently in the main branch

--- src/lop/probits/__init__.py
+++ src/lop/probits/__init__.py
@@ -1,6 +1,4 @@
 # init the probits subfolder
 
 from .ProbitBase import std_norm_pdf, std_norm_cdf, calc_pdf_cdf_ratio, ProbitBase
-# from .AbsBoundProbit import AbsBoundProbit
-# from .OrdinalProbit import OrdinalProbit
 from .PreferenceProbit import PreferenceProbit
