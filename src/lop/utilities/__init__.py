329569d01de070276577b2dc4235fe7e7d60267f
ianran <rankini@oregonstate.edu>
2024-01-29 18:23:54 -0800

    Added union function for splits and test functions. Fixed issues with broken split function as well

--- src/lop/utilities/__init__.py
+++ src/lop/utilities/__init__.py
@@ -1,7 +1,7 @@
 # init the utilities subfolder
 
 from .preference_pairs import get_dk, gen_pairs_from_idx, ranked_pairs_from_fake, generate_fake_pairs, generate_ranking_pairs
-from .training_utility import k_fold_split
+from .training_utility import k_fold_split, union_splits
 from .human_choice_model import p_human_choice, sample_human_choice
 from .pareto import get_pareto
 from .FakeFunction import FakeFunction, FakeLinear, FakeSquared, FakeLogistic, FakeSinExp
--- src/lop/utilities/training_utility.py
+++ src/lop/utilities/training_utility.py
@@ -22,6 +22,7 @@
 # A set of utility functions for training.
 
 import numpy as np
+import pdb
 
 ## k_fold_split
 # A function to split the datasets for training the PreferenceGP
@@ -30,27 +31,43 @@ import numpy as np
 def k_fold_split(y, k=2):
     data = []
 
-    for y_data in y:
+    for i, y_data in enumerate(y):
         if y_data is not None:
-            shuffle = np.arange(len(y_data))
-            np.random.shuffle(shuffle)
+            if i == 0:
+                shuffle = np.arange(len(y_data))
+                np.random.shuffle(shuffle)
+                splits = np.array_split(shuffle, k)
 
-            splits = np.array_split(shuffle, k)
+                # [probits[[split1, split2]], ...]
+                split_data = [[y_data[idx] for idx in split] for split in splits]
+                
+            elif i == 1 or i == 2:
+                shuffle = np.arange(len(y_data[0]))
+                np.random.shuffle(shuffle)
+                splits = np.array_split(shuffle, k)
 
-            # [probits[[split1, split2]], ...]
-            split_data = [[y_data[idx] for idx in split] for split in splits]
+                split_data = [[[y_data[0][idx], y_data[1][idx]] for idx in split] for split in splits]
+            else:
+                raise Exception("Not sure how to handle this split.")
 
             data.append(split_data)
+            
         else:
             data.append(None)
+
 
     actual_splits = []
     for j in range(k):
         split = []
 
         for i in range(len(y)):
-            if data[i] is not None:
-                split += [np.array(data[i][j])]
+            if data[i] is not None and len(data[i][j]) != 0:
+                if i == 0:
+                    split += [np.array(data[i][j])]
+                elif i == 1 or i == 2:
+                    arr0 = np.array([val[0] for val in data[i][j]])
+                    arr1 = np.array([val[1] for val in data[i][j]])
+                    split += [(arr0, arr1)]
             else:
                 split += [None]
 
@@ -58,3 +75,40 @@ def k_fold_split(y, k=2):
 
     return actual_splits
 
+def union_splits(splits, idx_to_combine):
+    import pdb
+    uni = []
+    for j in range(len(splits[0])):
+        if splits[0][j] is None:
+            uni.append(None)
+        elif isinstance(splits[0][j], np.ndarray):
+            if len(splits[0][j].shape) == 2:
+                uni.append(np.zeros((0,splits[0][j].shape[1])))
+            else:
+                raise Exception("Can't handle this union split with nd array")
+        elif isinstance(splits[0][j], tuple):
+            uni.append((np.array([]), np.array([])))
+        else:
+            pdb.set_trace()
+            raise Exception("Can't handle this split")
+
+    
+    
+    for i in range(len(splits)):
+        if uni[i] is not None:
+            for j in idx_to_combine:
+                if splits[i][j] is None:
+                    pass
+                elif i == 0:
+                    uni[i] = np.append(uni[i], splits[i][j], axis=0)
+                elif i == 1 or i == 2:
+                    uni[i] = (  np.append(uni[i][0], splits[j][i][0], axis=0), \
+                                np.append(uni[i][1], splits[j][i][1], axis=0))
+    if uni[1] is not None and len(uni[1][0]) == 0:
+        uni[1] = None
+    if uni[2] is not None and len(uni[2][0]) == 0:
+        uni[2] = None
+
+    return uni
+
+    
