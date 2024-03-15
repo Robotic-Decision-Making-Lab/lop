53cb973a2c94b7bf9456484ebebebedc03c1bd93
ianran <rankini@oregonstate.edu>
2024-03-15 11:42:38 -0700

    Updated experiment analysis and running multiple tests

--- experiments/run_multiple_tests.sh
+++ experiments/run_multiple_tests.sh
@@ -10,21 +10,23 @@ trap killgroup INT TERM
 
 # perfect, human_choice
 #user_type=human_choice
-number_runs=5
+number_runs=50
 #hyper_sel=no
 fake_func=linear
 
 for selc in SGV_UCB MUTUAL_INFO UCB RANDOM
 #for selc in UCB SGV_UCB MUTUAL_INFO MUTUAL_UCB
 do
-    #for fake_func in linear squared logistic sin_exp
-    for hyper_sel in hyper no
+    for fake_func in linear logistic squared sin_exp 
     do
-        for i_env in 0 1 2 3 4 5 6 7 8 9
+        for hyper_sel in no
         do
-            python3 single_experiment.py --env $i_env --model gp --selector $selc --sel_type choose1 --num_runs $number_runs --num_alts 4 --user perfect --hyper $hyper_sel --fake_func $fake_func &
+            for i_env in 0 1 2 3 4 5 6 7 8 9
+            do
+                python3 single_experiment.py --env $i_env --model linear --selector $selc --sel_type choose1 --num_runs $number_runs --num_alts 4 --user human_choice --hyper $hyper_sel --fake_func $fake_func &
+            done
+            wait
         done
-        wait
     done
 done
 
