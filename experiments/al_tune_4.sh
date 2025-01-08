e20a748a2c6041239afddc07c10afa9fb0b31ab9
ianran <rankini@oregonstate.edu>
2025-01-07 16:53:49 -0800

    Added new analysis plus code to run on laptop for testing methods further

---
+++ experiments/al_tune_4.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# The script to regenerate the results for the active learning figure in
+# the user-preference paper.
+
+
+killgroup(){
+    echo killing...
+    kill 0
+}
+
+trap killgroup INT TERM
+
+
+
+for i in {1..500}
+do
+    bash al_psynth_9_way_known_tune4.sh
+    sleep 5m
+done
+
