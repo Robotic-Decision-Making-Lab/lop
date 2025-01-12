13077e1376da13c893fc1b659f1031e2ed383249
ianran <rankini@oregonstate.edu>
2025-01-11 17:30:00 -0800

    Added tuning for min 0.8

---
+++ experiments/al_tune_12_lab.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# The script to regenerate the results for the active learning figure in
+# the user-preference paper.
+
+
+killgroup(){
+    echo killing...
+    kill 0
+}
+
+trap killgroup INT TERM
+
+
+
+for i in {1..500}
+do
+    echo "Starting main"
+    bash al_psynth_tune12_lab.sh
+    echo "Sleeping"
+    sleep 1m
+    echo "ending"
+done
+
